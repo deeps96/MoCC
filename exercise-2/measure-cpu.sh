de8fde91b9430e57c258cd1a507ca6e18514ec30
deeps96 <leonardo.huebscher@student.hpi.de>
2019-11-28 18:12:27 +0100

    Add benchmark scripts from exercise 1

---
+++ exercise-2/measure-cpu.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+EXECUTABLE="linpack"
+if [ ! -e $EXECUTABLE ] ; then
+        cc -O -o linpack linpack.c -lm
+fi
+
+# This function is based on: https://stackoverflow.com/questions/41750008/get-median-of-unsorted-array-in-one-line-of-bash
+function median(){
+  arr=($(printf '%lf\n' "${@}" | sort -n))
+  nel=${#arr[@]}
+  if (( $nel % 2 == 1 )); then     # Odd number of elements
+    val="${arr[ $(($nel/2)) ]}"
+  else                            # Even number of elements
+    (( j=nel/2 ))
+    (( k=j-1 ))
+    val=$(echo "scale=3;(${arr[j]}" + "${arr[k]})"/2|bc -l)
+  fi
+  echo $val
+}
+
+results=()
+start=$(date +"%s")
+current=$start
+diff=$(($current-$start))
+while (($diff < 20))
+do
+        result=$(./${EXECUTABLE} | tail -1 | sed "s/[[:blank:]]\+/ /g" | cut -d " " -f 7)
+        current=$(date +"%s")
+        diff=$(($current-$start))
+        results+=($result)
+done
+
+median "${results[@]}"
