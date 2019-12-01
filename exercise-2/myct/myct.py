f9ab80b95d8c26a4d83dd7f0bbe842d35eb2a073
constantin-lange <constantin.lange@student.hpi.uni-potsdam.de>
2019-12-01 17:14:48 +0100

    Add myct extension

--- exercise-2/myct/myct.py
+++ exercise-2/myct/myct.py
@@ -2,6 +2,7 @@ import fire # https://github.com/google/python-fire
 import wget
 import tarfile
 import os
+import time
 from sh import chroot, mount, unshare
 
 class MyCT(object):
@@ -20,9 +21,23 @@ class MyCT(object):
         # create dir if it does not exist
         mount('--bind', '-o', 'ro', hostPath, containerPath + targetPath)
 
-    def run(self, containerPath, executable, args='', namespace='kind=pid', limit='controllerkey=somekey'):
-        chroot(containerPath)
-        unshare(executable, args)
+    def run(self, containerPath, executable, args='', namespace=None, limit=None):
+        absolutePath = os.path.abspath(containerPath)
+        #sudo unshare --mount --pid --fork --mount-proc=$PWD/test/proc chroot ./test/ /bin/bash
+        command = 'unshare --mount --pid --fork --mount-proc='+absolutePath+'/proc chroot '+containerPath+' '+executable+' '+args
+        if not namespace is None:
+            command = 'nsenter --'+namespace.split('=')[0]+'=/proc/'+namespace.split('=')[1]+'/ns/'+namespace.split('=')[0]+' '+command  
+        current_time = str(int(round(time.time() * 1000)))
+        if not limit is None:
+            group = limit.split('=')[0].split('.')[0]
+            #os.system('mkdir /sys/fs/cgroup/'+group+'/'+current_time)
+            #os.system('echo '+limit.split('=')[1]+'>/sys/fs/cgroup/'+group+'/'+current_time+'/'+limit.split('=')[0])
+        os.system('mount -t proc proc '+absolutePath+'/proc')
+        os.system(command)
+        print(command)
+        os.system('wait $!')
+        os.system('umount '+absolutePath+'/proc')
+
 
 
 if __name__ == '__main__':
