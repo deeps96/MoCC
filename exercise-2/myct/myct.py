18071b115915e3d62ca1b576db07f1d8c823a396
constantin-lange <constantin.lange@student.hpi.uni-potsdam.de>
2019-12-01 19:30:06 +0100

    was auch immer

--- exercise-2/myct/myct.py
+++ exercise-2/myct/myct.py
@@ -2,6 +2,7 @@ import fire # https://github.com/google/python-fire
 import wget
 import tarfile
 import os
+import subprocess
 import time
 from sh import chroot, mount, unshare
 
@@ -26,17 +27,21 @@ class MyCT(object):
         #sudo unshare --mount --pid --fork --mount-proc=$PWD/test/proc chroot ./test/ /bin/bash
         command = 'unshare --mount --pid --fork --mount-proc='+absolutePath+'/proc chroot '+containerPath+' '+executable+' '+args
         if not namespace is None:
-            command = 'nsenter --'+namespace.split('=')[0]+'=/proc/'+namespace.split('=')[1]+'/ns/'+namespace.split('=')[0]+' '+command  
+            command = 'nsenter --'+namespace.split('=')[0]+'=/proc/'+namespace.split('=')[1]+'/ns/'+namespace.split('=')[0]+' '+command.replace('--pid ', '').replace('--mount ', '')  
         current_time = str(int(round(time.time() * 1000)))
         if not limit is None:
             group = limit.split('=')[0].split('.')[0]
-            #os.system('mkdir /sys/fs/cgroup/'+group+'/'+current_time)
-            #os.system('echo '+limit.split('=')[1]+'>/sys/fs/cgroup/'+group+'/'+current_time+'/'+limit.split('=')[0])
+            os.system('sudo mkdir /sys/fs/cgroup/'+group+'/'+current_time)
+            print('echo '+limit.split('=')[1]+' | sudo tee /sys/fs/cgroup/'+group+'/'+current_time+'/'+limit.split('=')[0])
+            os.system('echo '+limit.split('=')[1]+' | sudo tee /sys/fs/cgroup/'+group+'/'+current_time+'/'+limit.split('=')[0])
         os.system('mount -t proc proc '+absolutePath+'/proc')
+        #process = subprocess.call(command.split(' '))
         os.system(command)
-        print(command)
-        os.system('wait $!')
-        os.system('umount '+absolutePath+'/proc')
+        #os.system('echo $! | sudo tee /sys/fs/cgroup/'+group+'/'+current_time+'/tasks')
+        #os.system('wait $pid')
+        #os.system('umount '+absolutePath+'/proc')
+        #if not limit is None:
+        #    os.system('rmdir /sys/fs/cgroup/'+group+'/'+current_time)
 
 
 
